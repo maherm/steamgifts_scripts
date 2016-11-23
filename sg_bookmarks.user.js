54608bb44ecbe101ec3820ecd49c81a63b823fb1
mahermen <manuel.hermenau@gmail.com>
2016-11-24 00:07:35 +0100

    Make grayscale work again in webkit browsers

--- sg_bookmarks.user.js
+++ sg_bookmarks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SG Bookmarks
 // @namespace    http://steamgifts.com/
-// @version      0.2
+// @version      0.3
 // @description  Bookmark giveaways
 // @author       mahermen
 // @downloadURL  https://github.com/maherm/steamgifts_scripts/raw/master/sg_bookmarks.user.js
@@ -296,8 +296,8 @@ margin-right:10px;\
     width: 380px;\
 }\
 .__mh_ended{\
--webkit-filter: grayscale(100%);\
 filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale"); /* Firefox 3.5+ */ \
+-webkit-filter: grayscale(100%);\
 }\
 .__mh_bookmark_container .nav__row{\
 padding:0px;\
