65fc546798915545abc5a94afa5bb0dc97620ed3
mahermen <manuel.hermenau@gmail.com>
2016-12-18 13:32:15 +0100

    Only highlight blocks with a defined language

--- sg_syntax_highlighting.user.js
+++ sg_syntax_highlighting.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SG Syntax Highlighting
 // @namespace    https://www.steamgifts.com/user/mahermen
-// @version      0.2
+// @version      0.3
 // @description  Highlights source code
 // @require      https://code.jquery.com/jquery-3.1.1.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/scripts/shCore.min.js
@@ -17,7 +17,7 @@
 SgApi.Util.requireDeclaredStyles();
 SgApi.Util.injectCss(".syntaxhighlighter { overflow-y: hidden !important;padding-top: 8px!important;padding-bottom: 8px!important; }");
 
-$("pre code").each(function(i, block) {
+$("pre code[class]").each(function(i, block) {
     $(block).parent().addClass($(block).attr("class").replace("language-","toolbar:false; brush: ")).text($(block).text());
 });
 
